7a9fb0225c4cd633b97bd862dbd8ce0e7469abd7
Saloni Saroha <salonisaroha5@gmail.com>
2025-07-01 20:45:06 +0530

    setup Express server and connect backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+const chats = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require('./routes/userRoutes');
+const {notFound, errorHandler} =  require('./middlewares/errorMiddleware');
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+app.get('/', (req, res)=>{
+    res.send("API is running");
+});
+
+app.get('/api/chat', (req,res)=>{
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req,res)=>{
+    const singleChat =chats.find((c)=>c._id===req.params.id);
+    res.send(singleChat);
+});
+
+
+app.use('/api/user', userRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
+const PORT = process.env.PORT || 5000
+app.listen(5000, console.log(`Server is started on port ${PORT}`.yellow.bold));
