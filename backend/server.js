8d8a4a3c6583b2c3c57d6d131ebd732044bc8db6
Saloni Saroha <salonisaroha5@gmail.com>
2025-07-02 21:55:56 +0530

    made some changes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require("colors");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 const {notFound, errorHandler} =  require('./middlewares/errorMiddleware');
 const app = express();
 dotenv.config();
@@ -25,6 +26,7 @@ app.get('/api/chat/:id', (req,res)=>{
 
 
 app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
